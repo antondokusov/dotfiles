0f917ed530c740565d7e35372c452171c4fb1b89
Anton Dokusov <anton@gameram.com>
2024-07-19 21:15:15 +0400

    updated gitsigns config

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -97,16 +97,12 @@ return {
 		config = function()
 			local gitsigns = require 'gitsigns'
 			gitsigns.setup {
-
 				signs = {
-					add = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-					change = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-					delete = { hl = 'GitSignsDelete', text = '', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-					topdelete = { hl = 'GitSignsDelete', text = '', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-					changedelete = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-				},
-				current_line_blame_formatter_opts = {
-					relative_time = false,
+					add = { text = '▎' },
+					change = { text = '▎' },
+					delete = { text = '' },
+					topdelete = { text = '' },
+					changedelete = { text = '▎' },
 				},
 			}
 
