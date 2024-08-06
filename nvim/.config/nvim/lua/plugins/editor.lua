827484b3727b2776ef304e9e5967431c96721311
Anton Dokusov <anton@gameram.com>
2024-08-06 17:44:35 +0400

    removed Comment

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,14 +1,5 @@
 return {
   {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    config = function()
-      require('ts_context_commentstring').setup {
-        enable_autocmd = false,
-      }
-    end,
-  },
-
-  {
     'nvim-treesitter/nvim-treesitter-context',
     opts = {},
   },
@@ -30,18 +21,6 @@ return {
             node_decremental = '<S-TAB>',
           },
         },
-      }
-    end,
-  },
-
-  {
-    'numToStr/Comment.nvim',
-    dependencies = {
-      'JoosepAlviste/nvim-ts-context-commentstring',
-    },
-    config = function()
-      require('Comment').setup {
-        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
       }
     end,
   },
