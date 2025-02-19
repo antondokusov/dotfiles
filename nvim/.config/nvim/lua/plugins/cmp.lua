e3d33803fa77885043c659457da44cd64a7ab411
Anton Dokusov <anton@gameram.com>
2025-02-19 11:30:31 +0400

    chore(nvim cmp): clean up

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -74,13 +74,7 @@ return {
 			{ '<s-tab>', false, mode = { 'i', 's' } },
 		},
 		config = function()
-			local has_words_before = function()
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-			end
-
 			local cmp = require 'cmp'
-			local luasnip = require 'luasnip'
 
 			cmp.setup {
 				snippet = {
