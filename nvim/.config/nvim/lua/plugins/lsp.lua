83467fad29061c9020f12ae2abf9632a13ecb7de
Anton Dokusov <anton@gameram.com>
2025-08-17 15:26:50 +0200

    nvim: removed diagnostics signs

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -42,22 +42,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 local function setup()
-  local signs = {
-    { name = 'DiagnosticSignError', text = '' },
-    { name = 'DiagnosticSignHint', text = '' },
-    { name = 'DiagnosticSignWarn', text = '' },
-    { name = 'DiagnosticSignInfo', text = '' },
-  }
-
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
-  end
-
   local config = {
     virtual_text = true,
-    signs = {
-      active = signs,
-    },
     update_in_insert = true,
     underline = true,
     severity_sort = true,
