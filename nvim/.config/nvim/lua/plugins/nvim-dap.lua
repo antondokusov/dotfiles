f54a5d04fec1469267f13be98f1b492849c999fc
Anton Dokusov <anton@gameram.com>
2026-02-21 16:37:50 +0100

    nvim: removed python dap configuration, added dart release launches

--- nvim/.config/nvim/lua/plugins/nvim-dap.lua
+++ nvim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -1,21 +1,5 @@
 vim.pack.add({ 'https://github.com/mfussenegger/nvim-dap.git' })
 
-local python = {
-  adapter = {
-    type = 'executable',
-    command = 'python3',
-    args = { '-m', 'debugpy.adapter' },
-  },
-  configurations = {
-    {
-      type = 'python',
-      request = 'launch',
-      name = 'Launch file',
-      program = '${file}',
-    },
-  },
-}
-
 -- https://github.com/flutter/flutter/blob/master/packages/flutter_tools/lib/src/debug_adapters/README.md#launchattach-arguments
 local dart = {
   adapter = {
@@ -42,14 +26,27 @@ local dart = {
         cwd = '${workspaceFolder}',
         toolArgs = { '--flavor', 'production', '-d', device, '--dart-define-from-file=api_keys.json' },
       },
+      {
+        type = 'dart',
+        request = 'launch',
+        name = 'Development Release (' .. device .. ')',
+        program = 'lib/main_development.dart',
+        cwd = '${workspaceFolder}',
+        toolArgs = { '--flavor', 'development', '-d', device, '--dart-define-from-file=api_keys.json', '--release' },
+      },
+      {
+        type = 'dart',
+        request = 'launch',
+        name = 'Production Release (' .. device .. ')',
+        program = 'lib/main_production.dart',
+        cwd = '${workspaceFolder}',
+        toolArgs = { '--flavor', 'production', '-d', device, '--dart-define-from-file=api_keys.json', '--release' },
+      },
     }
   end,
 }
 
 local dap = require 'dap'
-
-dap.adapters.python = python.adapter
-dap.configurations.python = python.configurations
 
 dap.adapters.dart = dart.adapter
 dap.providers.configs['dart'] = dart.configurations_provider
