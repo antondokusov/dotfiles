4ed4e247759bea99eb5e90a9bf3bfcb575188e86
Anton Dokusov <anton@gameram.com>
2026-02-21 16:38:55 +0100

    nvim: some changes in colorscheme and options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,5 @@
+local function list(items, sep) return table.concat(items, sep or ',') end
+
 vim.g.mapleader = ' '
 
 vim.scriptencoding = 'utf-8'
@@ -7,6 +9,16 @@ vim.opt.fileencoding = 'utf-8'
 vim.opt.termguicolors = true
 vim.opt.mouse = 'a'
 vim.opt.clipboard = 'unnamedplus'
+-- vim.opt.shellcmdflag = '-ci'
+
+-- Automatically indents new lines to align with the previous line’s indentation level
+vim.opt.autoindent = true
+
+-- Sets the number of spaces used for each indentation level when using commands like >> or <<
+vim.opt.shiftwidth = 2
+
+-- Defines the width of a tab character in spaces when displayed.
+vim.opt.tabstop = 2
 
 vim.opt.backup = false
 vim.opt.writebackup = false
@@ -24,11 +36,9 @@ vim.opt.timeoutlen = 500
 vim.opt.undofile = true
 vim.opt.updatetime = 300
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
 vim.opt.cursorline = true
 vim.opt.number = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.opt.numberwidth = 4
 vim.opt.signcolumn = 'yes'
 vim.opt.wrap = false
@@ -36,12 +46,32 @@ vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.opt.shortmess:append 'c'
 vim.opt.laststatus = 3
+-- vim.opt.statusline = "%#Normal#  %#StatusLine#%f %m%r%h%w %=%-14.(%l,%c%V%)%=%P%#Normal#"
+vim.opt.smoothscroll = true -- todo
+vim.opt.listchars = list {
+  'tab: ──',
+  'lead:·',
+  'trail:•',
+  'nbsp:␣',
+  'precedes:«',
+  'extends:»',
+}
+vim.opt.fillchars = list {
+  'vert:│',
+  'diff:╱',
+  'foldclose:›',
+  'foldopen:',
+  'fold: ',
+  'msgsep:─',
+}
 
-vim.o.foldcolumn = '0'
-vim.o.foldlevel = 99
+vim.o.diffopt = 'internal,filler,closeoff,indent-heuristic,linematch:60,algorithm:histogram'
+vim.o.foldmethod = 'expr'
+-- vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.foldexpr = "v:lnum==1?'>1':getline(v:lnum)=~'import'?1:nvim_treesitter#foldexpr()"
 vim.o.foldlevelstart = 99
-vim.o.foldenable = true
 
-vim.o.winborder = 'rounded'
+vim.opt.winborder = 'rounded'
 
 vim.cmd 'set nomodeline'
--- nvim/.config/nvim/lua/plugins_lazy/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins_lazy/colorscheme.lua
@@ -1,16 +1,14 @@
 return {
 	'rose-pine/neovim',
+	dir = '~/Documents/rose-pine/',
 	lazy = false,
 	priority = 1000,
 	config = function()
-    vim.cmd 'set bg=light'
+		vim.cmd 'set bg=light'
 
 		require('rose-pine').setup {
 			variant = 'dawn',
-			-- disable_background = true,
 			disable_float_background = true,
 		}
-
-		vim.cmd 'colorscheme rose-pine-dawn'
 	end,
 }
