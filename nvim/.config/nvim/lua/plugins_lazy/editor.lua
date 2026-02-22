98585ae6349588a770d086be1f61c765dbcf771e
Anton Dokusov <anton@gameram.com>
2026-02-22 19:13:27 +0100

    nvim: cleanup

--- nvim/.config/nvim/lua/plugins_lazy/editor.lua
+++ nvim/.config/nvim/lua/plugins_lazy/editor.lua
@@ -12,56 +12,11 @@ return {
           inline = "chars",
         },
       },
-      -- add any options here
       cli = {
         mux = {
           backend = "zellij",
           enabled = true,
         },
-      },
-    },
-    keys = {
-      {
-        '<leader>aa',
-        function() require('sidekick.cli').toggle() end,
-        desc = 'Sidekick Toggle CLI',
-      },
-      {
-        '<leader>as',
-        function() require('sidekick.cli').select() end,
-        -- Or to select only installed tools:
-        -- require("sidekick.cli").select({ filter = { installed = true } })
-        desc = 'Select CLI',
-      },
-      {
-        '<leader>at',
-        function() require('sidekick.cli').send { msg = '{this}' } end,
-        mode = { 'x', 'n' },
-        desc = 'Send This',
-      },
-      {
-        '<leader>av',
-        function() require('sidekick.cli').send { msg = '{selection}' } end,
-        mode = { 'x' },
-        desc = 'Send Visual Selection',
-      },
-      {
-        '<leader>ap',
-        function() require('sidekick.cli').prompt() end,
-        mode = { 'n', 'x' },
-        desc = 'Sidekick Select Prompt',
-      },
-      {
-        '<c-.>',
-        function() require('sidekick.cli').focus() end,
-        mode = { 'n', 'x', 'i', 't' },
-        desc = 'Sidekick Switch Focus',
-      },
-      -- Example of a keybinding to open Claude directly
-      {
-        '<leader>ac',
-        function() require('sidekick.cli').toggle { name = 'claude', focus = true } end,
-        desc = 'Sidekick Toggle Claude',
       },
     },
   },
@@ -204,25 +159,6 @@ return {
         },
       },
     },
-  },
-
-  {
-    'kevinhwang91/nvim-bqf',
-    dependencies = { 'junegunn/fzf' },
-    ft = 'qf',
-    enabled = false,
-    config = function()
-      require('bqf').setup {
-        func_map = {
-          fzffilter = '/',
-        },
-        filter = {
-          fzf = {
-            extra_opts = { '--bind', 'ctrl-o:toggle-all', '--delimiter', '│' },
-          },
-        },
-      }
-    end,
   },
 
   {
