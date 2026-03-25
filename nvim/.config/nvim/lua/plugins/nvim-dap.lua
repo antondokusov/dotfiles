1d21a3c0f36304d597d8866a168dc7df5a682093
Anton Dokusov <anton.dokusov@protonmail.com>
2026-03-25 14:53:55 +0100

    feat: show ▶️ icon on zellij tab during DAP session

--- nvim/.config/nvim/lua/plugins/nvim-dap.lua
+++ nvim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -52,3 +52,27 @@ dap.adapters.dart = dart.adapter
 dap.providers.configs['dart'] = dart.configurations_provider
 dap.defaults.dart.exception_breakpoints = {}
 dap.defaults.fallback.exception_breakpoints = {}
+
+-- Zellij tab icon: show ▶️ prefix while DAP session is active
+local zellij_tab_icon = require('util.zellij-tab-icon')
+local zellij_dap_handle = nil
+
+dap.listeners.after.event_initialized['zellij-tab-icon'] = function()
+  if zellij_dap_handle then return end
+  zellij_dap_handle = zellij_tab_icon.add('▶️ ')
+end
+
+local function on_dap_end()
+  if not zellij_dap_handle then return end
+  zellij_tab_icon.remove(zellij_dap_handle)
+  zellij_dap_handle = nil
+end
+
+dap.listeners.after.event_terminated['zellij-tab-icon'] = on_dap_end
+dap.listeners.after.event_exited['zellij-tab-icon'] = on_dap_end
+
+local group = vim.api.nvim_create_augroup('zellij-dap-tab-icon', { clear = true })
+vim.api.nvim_create_autocmd('VimLeavePre', {
+  group = group,
+  callback = on_dap_end,
+})
